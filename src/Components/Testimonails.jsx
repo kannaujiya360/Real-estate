cd268adcc4e96d1be0a90baffa54ff2e28a3d452
sunil <skannaujiya920@gmail.com>
2025-01-26 10:14:26 +0530

    changes

--- src/Components/Testimonails.jsx
+++ src/Components/Testimonails.jsx
@@ -12,7 +12,7 @@ const Testimonails = () => {
     
     
     className='container mx-auto py-10 lg:px-32 w-full overflow-hidden ' id='Testimonials'>
-      <h1 className='text-2xl sm:text-4xl font-bold mb-2 text-center'> Customer <span className='underline underline-offset-4 decoration-1 under font-light'> Testimonails</span></h1>
+      <h1 className='text-2xl sm:text-4xl font-bold mb-2 text-center'> Customer <span className='underline underline-offset-4 decoration-1 under font-light'> Testimonials</span></h1>
       <p className='text-center text-gray-500 mb-12 max-w-80 mx-auto'>Real Stories From Those WhonFound Home with Us</p>
 
       <div className=' flex flex-wrap justify-center  gap-4'>
